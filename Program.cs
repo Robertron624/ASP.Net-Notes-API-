16531ab6ac08c27776d11017fedb848a9eeff151
Roberto Ramirez <robert.624@hotmail.com>
2022-08-28 15:28:33 -0500

    CRUD routes added

--- Program.cs
+++ Program.cs
@@ -22,6 +22,52 @@ if(app.Environment.IsDevelopment()){
 app.MapGet("/", () => "Welcome to Notes API");
 
 
+// -------------------------------------------ROUTES----------------------------------------
+
+//Get all Notes
+app.MapGet("/notes", async (NoteDb db) => await db.Notes.ToListAsync());
+
+//Create new Note 
+app.MapPost("/notes/", async (Note n, NoteDb db) => {
+    db.Add(n);
+    await db.SaveChangesAsync();
+    return Results.Created($"/notes/{n.id}", n);
+});
+//Get Note by Id 
+app.MapGet("/notes/{id:int}", async (int id, NoteDb db)=>{
+    return await db.Notes.FindAsync(id)
+            is Note n
+            ? Results.Ok(n)
+            : Results.NotFound();
+});
+//Update Note by Id
+app.MapPut("/notes/{id:int}", async (NoteDb db,Note n, int id)=>{
+    if(n.id != id){
+        return Results.BadRequest();
+    }
+    var note  = await db.Notes.FindAsync(id);
+    if (note is null) return Results.NotFound();    
+
+    note.text = n.text;
+    note.done = n.done;
+    await db.SaveChangesAsync();
+    return Results.Ok(note);
+});
+
+//Delete Note by Id
+app.MapDelete("/notes/{id:int}", async (NoteDb db, int id)=>{
+
+    var note  = await db.Notes.FindAsync(id);
+    if (note is not null){
+        db.Notes.Remove(note);
+        await db.SaveChangesAsync();
+    };    
+
+    return Results.NoContent();
+});
+
+
+// app initiation
 await app.RunAsync();
 
 
@@ -31,7 +77,7 @@ record Note(int id){
 }
 
 
-
+// DB Context
 class NoteDb: DbContext {
     public NoteDb(DbContextOptions<NoteDb> options): base(options) {
 
