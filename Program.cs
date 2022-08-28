4922f77278a0e3b47cc86b990de765ed9bcdd18d
Roberto Ramirez <robert.624@hotmail.com>
2022-08-27 21:26:52 -0500

    connection to db and migrations done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder  = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<NoteDb>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var  app = builder.Build();
+
+if(app.Environment.IsDevelopment()){
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 
 app.MapGet("/", () => "Welcome to Notes API");
 
+
 await app.RunAsync();
+
+
+record Note(int id){
+    public string text {get;set;} = default!;
+    public bool done {get;set;} = default!;
+}
+
+
+
+class NoteDb: DbContext {
+    public NoteDb(DbContextOptions<NoteDb> options): base(options) {
+
+    }
+    public DbSet<Note> Notes => Set<Note>();
+}
